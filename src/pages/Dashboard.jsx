400b5e5cd920d10b57186895018fa8d585d89832
Krima Madhikarmi <madhikrima20@gmail.com>
2024-10-21 15:53:43 +0545

    dashboard content

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,27 @@
-import Navbar from "../components/Navbar";
+import Navbar from '../components/Navbar';
+import '../styles/Dashboard.css';
 
-const DashBoard=()=>{
-    return(
-        <>
-        <Navbar/>
-        </>
-    )
-}
+const DashBoard = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="dashContent">
+        <div className="content1">
+          <div className="box box1">
+            Todays Event
+            <div className="maincontent">
+              <p> No content</p>
+            </div>
+          </div>
+          <div className=" box box2">Academic Calendar</div>
+        </div>
+        <div className="content2">Upcoming Event
+        <div className="maincontent">
+              <p> No content</p>
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default DashBoard;
