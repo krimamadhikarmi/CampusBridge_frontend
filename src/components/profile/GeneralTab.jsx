a1a57894e42d54451ea391498f7b37a1035510f2
Krima Madhikarmi <madhikrima20@gmail.com>
2024-10-23 14:57:14 +0545

    designed profile

--- src/components/profile/GeneralTab.jsx
+++ src/components/profile/GeneralTab.jsx
@@ -1,13 +1,23 @@
 const GeneralTab = () => {
   return (
-    <div className="tab-style">
+    <div className="generaltab-style">
       <div>
-        <div className="profile-picture">
-          <img src="girl.webp" alt="girl" width={140} />
+        <div className="header-part">
+          <div className="profile-picture">
+            <img src="girl.webp" alt="girl" width={140} />
+          </div>
+          <div className="introduction">
+            <h2>Krima Madhikarmi</h2>
+            <p> Bsc.CSIT 7th Semester</p>
+          </div>
         </div>
-        <div className="introduction">
-          Krima Madhikarmi<br/>
-           Bsc.CSIT 7th Semester
+
+        <div className="profile-details">
+          <p><b>College:</b></p>
+          <p><b>Batch</b>:</p>
+          <p><b>Address:</b></p>
+          <p><b>Phone:</b></p>
+          <p><b>Email:</b></p>
         </div>
       </div>
     </div>
