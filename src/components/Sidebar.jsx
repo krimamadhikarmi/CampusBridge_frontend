f0cd9eaff5e59b790d449656d4f212a6ca2c8f37
Krima Madhikarmi <madhikrima20@gmail.com>
2024-10-23 10:38:32 +0545

    side bar added in profile page

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import '../styles/Profile.css'
+const Sidebar=()=>{
+  return(
+    <div className="sidebar">
+      <p>General</p>
+      <p>Attendance</p>
+      <p>My Clubs</p>
+      <p>Account</p>
+    </div>
+  )
+}
+export default Sidebar;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,11 @@
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const Profile=()=>{
     return(
         <>
         <Navbar/>
-        <h1>Profile page</h1>
+        <Sidebar/>
         </>
     )
 }
