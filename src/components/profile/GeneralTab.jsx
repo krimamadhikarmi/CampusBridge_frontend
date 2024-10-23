38c95e208fe06a3270bb37aa850d4e7a2f6f10d3
Krima Madhikarmi <madhikrima20@gmail.com>
2024-10-23 22:04:16 +0545

    profile design changed

--- src/components/profile/GeneralTab.jsx
+++ src/components/profile/GeneralTab.jsx
@@ -3,21 +3,38 @@ const GeneralTab = () => {
     <div className="generaltab-style">
       <div>
         <div className="header-part">
+          <h2>Your Profile</h2>
           <div className="profile-picture">
             <img src="girl.webp" alt="girl" width={140} />
           </div>
-          <div className="introduction">
+          {/* <div className="introduction">
             <h2>Krima Madhikarmi</h2>
             <p> Bsc.CSIT 7th Semester</p>
-          </div>
+          </div> */}
         </div>
 
         <div className="profile-details">
-          <p><b>College:</b></p>
-          <p><b>Batch</b>:</p>
-          <p><b>Address:</b></p>
-          <p><b>Phone:</b></p>
-          <p><b>Email:</b></p>
+          <p>
+            <b>Name:</b> Krima Madhikarmi
+          </p>
+          <p>
+            <b>Faculty:</b> Bsc.CSIT
+          </p>
+          <p>
+            <b>College:</b> Samriddhi College
+          </p>
+          <p>
+            <b>Batch:</b> 2075
+          </p>
+          <p>
+            <b>Address:</b> Suryabinayak,Bhaktapur
+          </p>
+          <p>
+            <b>Phone:</b> 98433838828
+          </p>
+          <p>
+            <b>Email:</b> madhikrima20@gmail.com
+          </p>
         </div>
       </div>
     </div>
