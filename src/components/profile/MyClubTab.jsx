4045125b9163e70a07e9141d018bbadaf2e0fce0
Krima Madhikarmi <madhikrima20@gmail.com>
2024-10-25 23:04:19 +0545

    my club tab created and designed for data display

--- src/components/profile/MyClubTab.jsx
+++ src/components/profile/MyClubTab.jsx
@@ -1,8 +1,60 @@
-const MyClubTab=()=>{
-    return(
-        <div className="tab-style">
-        <h1> My Club Tab</h1>
+const MyClubTab = () => {
+  return (
+    <div className="tab-style">
+      <div className="tab-header">
+        <h1> My Clubs</h1>
+      </div>
+      <div className="clubs-container">
+        <div className="club-card">
+          <div className="club-title">Sports Club</div>
+          <div className="club-image">
+            <img src="images.png" alt="ball" />
+          </div>
+          <div className="view-button">
+            <button className="club-button">View</button>
+          </div>
         </div>
-    )
-}
+        <div className="club-card">
+          <div className="club-title">Club 2</div>
+          <div className="club-image">
+            <img src="sports.jpeg" alt="ball" />
+          </div>
+          <div className="view-button">
+            <button className="club-button">View</button>
+          </div>
+        </div>
+        <div className="club-card">
+          {' '}
+          <div className="club-title">Club 3</div>
+          <div className="club-image">
+            <img src="images.png" alt="ball" />
+          </div>
+          <div className="view-button">
+            <button className="club-button">View</button>
+          </div>
+        </div>
+        <div className="club-card">
+          {' '}
+          <div className="club-title">Club 4</div>
+          <div className="club-image">
+            <img src="images.png" alt="ball" />
+          </div>
+          <div className="view-button">
+            <button className="club-button">View</button>
+          </div>
+        </div>
+        <div className="club-card">
+          {' '}
+          <div className="club-title">Club 5</div>
+          <div className="club-image">
+            <img src="images.png" alt="ball" />
+          </div>
+          <div className="view-button">
+            <button className="club-button">View</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default MyClubTab;
