dfcb68ab2b0d5578b594569054a56068a7387a46
Krima Madhikarmi <madhikrima20@gmail.com>
2024-10-27 12:17:14 +0545

    help page designing

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,11 +1,18 @@
-import Navbar from "../components/Navbar";
+import Navbar from '../components/Navbar';
+import '../styles/Help.css';
 
-const Help=()=>{
-    return(
-        <>
-        <Navbar/>
-        <h1>Help page</h1>
-        </>
-    )
-}
+const Help = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="help-header">
+        <h2>Ask & Assit</h2>
+      </div>
+      <div className="help-box">
+        <div className='question-box'>question form</div>
+        <div className='rules-box'>Rules</div>
+      </div>
+    </>
+  );
+};
 export default Help;
