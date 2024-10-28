ffdebc08c9888f480dbe9c4e890f5107319b2fbd
Krima Madhikarmi <madhikrima20@gmail.com>
2024-10-28 10:22:46 +0545

    navigating to dashboard when on clicking on login button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,34 @@
 import { useState } from 'react';
 import CustomFormField from '../components/customFormField';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
+
 import '../styles/LoginStyle.css';
 const Login = () => {
-  const [email,setEmail]=useState('');
-  const [password,setPassword]=useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-  const handleSubmit=( event)=>{
+  const handleSubmit = (event) => {
     event.preventDefault();
-    if(email.length===0){
-      alert('Please enter the email')
+    if (email.length === 0) {
+      alert('Please enter the email');
       return;
     }
-    if(password.length===0){
-      alert('Please enter the password')
+    if (password.length === 0) {
+      alert('Please enter the password');
       return;
     }
-    console.log('email',email);
-    console.log('password',password)
-  }
+    console.log('email', email);
+    console.log('password', password);
+    navigate('/dashboard');
+  };
 
-  const handleEmail=(event)=>{
+  const handleEmail = (event) => {
     setEmail(event.target.value);
-  }
-  const handlePassword=(event)=>{
+  };
+  const handlePassword = (event) => {
     setPassword(event.target.value);
-  }
+  };
   return (
     <>
       <div id="loginBox">
@@ -33,14 +37,22 @@ const Login = () => {
         </div>
         <div>
           <form onSubmit={handleSubmit}>
-            <CustomFormField label="Email" cname="Email" placeholder="Enter your email" type="email" 
-            value={email}
-            onChange={handleEmail}
+            <CustomFormField
+              label="Email"
+              cname="Email"
+              placeholder="Enter your email"
+              type="email"
+              value={email}
+              onChange={handleEmail}
             />
-            <CustomFormField label="Password" cname="password" placeholder="Enter your password" type="password"
-            value={password}
-            onChange={handlePassword}
-             />
+            <CustomFormField
+              label="Password"
+              cname="password"
+              placeholder="Enter your password"
+              type="password"
+              value={password}
+              onChange={handlePassword}
+            />
 
             <div id="login-button">
               <input type="submit" />
