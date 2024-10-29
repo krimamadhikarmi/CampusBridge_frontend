306d664e9b792f42215d2f1f12ee9dacf9bf042f
Krima Madhikarmi <madhikrima20@gmail.com>
2024-10-29 10:32:11 +0545

    result  info added in result page

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -10,6 +10,17 @@ const Result = () => {
       <Navbar />
       <PageHeader pageTitle={'Result'} />
       <div className="result-box">
+        <div className="result-info">
+          <p> Note: One Credit Hour equals 32 Clock Hours </p>
+          <p>TH: THEORY </p>
+          <p>PR: PRACTICAL </p>
+          <p>Abs: ABSENT </p>
+          <p> W: WITHHELD </p>
+          <p>
+            This sheet is for general ideas of grade(s) you secured.This is not for official appear. If any mistakes
+            appear; record at respective college administration or University will be refered.
+          </p>
+        </div>
         {hasResult ? (
           <div className="result-present">
             <table className="result-table">
