1d56b10eec798bbb050f656b8bab6c5ee00b4c1d
Krima Madhikarmi <madhikrima20@gmail.com>
2024-10-30 22:28:41 +0545

    side bar and display area added in syllabus page

--- src/pages/Syllabus.jsx
+++ src/pages/Syllabus.jsx
@@ -1,11 +1,23 @@
 import Navbar from '../components/Navbar';
 import PageHeader from '../components/PageHeader';
+import '../styles/Syllabus.css';
 
 const Syllabus = () => {
   return (
     <>
       <Navbar />
       <PageHeader pageTitle={'Syllabus'} />
+      <div className="syllabus-box">
+        <div className="syllabus-side-bar">
+          <p>Advanced Java Programming</p>
+          <p>Data WareHousing and Data Mining</p>
+          <p>Principle of Management</p>
+          <p>Project Work</p>
+          <p>Electives</p>
+          
+        </div>
+        <div className="syllabus-content"></div>
+      </div>
     </>
   );
 };
