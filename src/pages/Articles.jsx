1e266ceb2be74bcd6158fdcdeb376a5af171737d
Krima Madhikarmi <madhikrima20@gmail.com>
2024-10-30 12:10:36 +0545

    commit

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import PageHeader from '../components/PageHeader';
 import '../styles/Articles.css';
 import ArticleList from '../components/ArticleList';
+import CustomFormField from '../components/customFormField';
 
 const Articles = () => {
-  //created a fake api for testing purpose
+  const [dropdown, setDropDown] = useState(false);
+  const [headline, setHeadLine] = useState('');
+  const [description, setDescription] = useState('');
+  const [tag, setTag] = useState('');
+  const [tagLine, setTagLine] = useState('');
+
+  const [currentDate, setCurrentDate] = useState('');
+
+  useEffect(() => {
+    const today = new Date().toISOString().split('T')[0];
+    setCurrentDate(today);
+  }, []);
+
+  const toggleDown = () => {
+    setDropDown(!dropdown);
+  };
+
+  const handleSubmit = () => {
+    console.log('title', headline);
+    console.log('description', description);
+    console.log('tag', tag);
+    console.log('tagline', tagLine);
+    console.log('date', currentDate);
+  };
+
+  const handleTag = (event) => {
+    setTag(event.target.value);
+  };
+
+  const handleTitle = (event) => {
+    setHeadLine(event.target.value);
+  };
+  const handleTagLine = (event) => {
+    setTagLine(event.target.value);
+  };
+  const handleDescription = (event) => {
+    setDescription(event.target.value);
+  };
+
   const articles = [
     {
       id: 1,
       title: '5 Amazing New JavaScript Features in ES15 (2024)',
       description: '5 juicy ES15 features with new functionality for cleaner and shorter JavaScript code in 2024.',
       author: 'Tari Ibaba',
-
       date: 'Jun 2',
-
       imageUrl: 'sports.jpeg',
     },
     {
@@ -22,9 +59,7 @@ const Articles = () => {
       title: 'React Native’s New Architecture: The Tricky Parts (2/2)',
       description: 'The first part ended with you implementing a custom Shadow component.',
       author: 'Jakub Piasecki',
-
       date: 'Jun 10',
-
       imageUrl: 'images.png',
     },
   ];
@@ -34,6 +69,9 @@ const Articles = () => {
       <Navbar />
       <PageHeader pageTitle={'Articles'} />
       <div className="article-box">
+        <div className="article-form">
+          <button onClick={toggleDown}>Add article</button>
+        </div>
         <div className="article-list">
           {articles.map((article) => (
             <div key={article.id} className="article-item">
@@ -48,6 +86,43 @@ const Articles = () => {
           ))}
         </div>
       </div>
+      {dropdown && (
+        <form onSubmit={handleSubmit}>
+          <div className={`article-form-box ${dropdown ? 'show' : ''}`}>
+            <CustomFormField label={'Tag'} name={'tag'} type={'text'} value={tag} onChange={handleTag} />
+            <CustomFormField
+              label={'Headline'}
+              name={'headline'}
+              type={'text'}
+              value={headline}
+              onChange={handleTitle}
+            />
+            <CustomFormField
+              label={'Tagline'}
+              name={'tagline'}
+              type={'text'}
+              value={tagLine}
+              onChange={handleTagLine}
+            />
+            <CustomFormField
+              label={'Description'}
+              name={'description'}
+              type={'text'}
+              value={description}
+              onChange={handleDescription}
+            />
+            <CustomFormField label={'Date'} name={'date'} type={'date'} value={currentDate} />
+            <div className="button-group">
+              <button type="submit" className="submit-button">
+                Save Changes
+              </button>
+              <button type="submit" className="cancel-button" onClick={toggleDown}>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </form>
+      )}
     </>
   );
 };
