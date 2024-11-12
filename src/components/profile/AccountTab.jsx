2092e2cf68e3e2a7791317d0c86621150ed053f5
Krima Madhikarmi <madhikrima20@gmail.com>
2024-11-12 14:15:48 +0545

    scholarship data added

--- src/components/profile/AccountTab.jsx
+++ src/components/profile/AccountTab.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
 const AccountTab = () => {
+  const studentData = {
+    name: 'Krima Madhikarmi',
+    faculty: 'BSc.CSIT',
+    semester: '7th',
+    totalFee: 95000,
+    // scholarshipAmount: 300,
+    paymentStatus: 'Paid',
+    dueDate: '2024-12-01',
+  };
   return (
-    <div className="tab-style">
+    <div className="account-tab-style">
       <div className="account-header">
         <h2>Semester Fee</h2>
       </div>
 
       <div className="student-info">
-        <p>Name: Krima Madhikarmi</p>
-        <p>Semester: 7th Semester</p>
-        <p>Faculty: BSc.CSIT</p>
+        <p>Name: {studentData.name}</p>
+        <p>Semester: {studentData.semester}</p>
+        <p>Faculty:{studentData.faculty}</p>
       </div>
 
       <div className="fee-table">
@@ -24,16 +33,29 @@ const AccountTab = () => {
           <tbody>
             <tr>
               <td>Semester Fee</td>
-              <td>95,000</td>
+              <td>{studentData.totalFee}</td>
+            </tr>
+            <tr>
+              <td>Scholarship Fee</td>
+              <td>{studentData.scholarshipAmount ? studentData.scholarshipAmount : 0}</td>
             </tr>
           </tbody>
           <tfoot>
             <tr>
               <td>Total Fee</td>
-              <td>95,000</td>
+              <td>
+                {studentData.scholarshipAmount
+                  ? studentData.totalFee - studentData.scholarshipAmount
+                  : studentData.totalFee}
+              </td>
             </tr>
           </tfoot>
         </table>
+      </div>
+      <div className="fee-instruction">
+        <p className="duedate">Due Date: {studentData.dueDate}</p>
+        {/* <p className='status'>Status: {studentData.paymentStatus}</p> */}
+        <p className={`status ${studentData.paymentStatus.toLowerCase()}`}>Status: {studentData.paymentStatus}</p>
       </div>
     </div>
   );
