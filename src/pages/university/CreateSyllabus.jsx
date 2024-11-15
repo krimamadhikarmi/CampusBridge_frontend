dfb5764dded5ec636824b229240a6e0f40d14f25
Krima Madhikarmi <madhikrima20@gmail.com>
2024-11-15 14:01:56 +0545

    added button

--- src/pages/university/CreateSyllabus.jsx
+++ src/pages/university/CreateSyllabus.jsx
@@ -7,11 +7,17 @@ const CreateSyllabus = () => {
       <div className="syllabus-body">
         <div className="display-div">
           <h2> Syllabus</h2>
+          <div className="button-container">
+            <button className='add-button'>Add Syllabus</button>
+          </div>
           <div className="no-data-list">No Syllabus Yet</div>
         </div>
         <hr className="divider" />
         <div className="display-div">
           <h2>Courses</h2>
+          <div className="button-container">
+            <button className='add-button'>Add Courses</button>
+          </div>
           <div className="no-data-list">No Courses Yet</div>
         </div>
       </div>
