0c5d5eab735d65de645519ab089b2e66d3dec5d2
Krima Madhikarmi <madhikrima20@gmail.com>
2024-11-15 21:32:40 +0545

    create syllabus popup

--- src/pages/university/CreateSyllabus.jsx
+++ src/pages/university/CreateSyllabus.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import Navbar from '../../components/Navbar';
+import CloseButton from '../../components/CloseButton';
 
 const CreateSyllabus = () => {
+  const [toogleForm, setToogleForm] = useState(false);
+
+  const handleSyllabusForm = () => {
+    setToogleForm(!toogleForm);
+  };
   return (
     <>
       <Navbar />
@@ -8,7 +15,9 @@ const CreateSyllabus = () => {
         <div className="display-div">
           <h2> Syllabus</h2>
           <div className="button-container">
-            <button className='add-button'>Add Syllabus</button>
+            <button className="add-button" onClick={handleSyllabusForm}>
+              Add Syllabus
+            </button>
           </div>
           <div className="no-data-list">No Syllabus Yet</div>
         </div>
@@ -16,11 +25,21 @@ const CreateSyllabus = () => {
         <div className="display-div">
           <h2>Courses</h2>
           <div className="button-container">
-            <button className='add-button'>Add Courses</button>
+            <button className="add-button">Add Courses</button>
           </div>
           <div className="no-data-list">No Courses Yet</div>
         </div>
       </div>
+      {toogleForm && (
+        <div className="form-overlay">
+          <div className="syllabus-form" onClick={(e) => e.stopPropagation()}>
+            <div className="syllabus-form-header">
+              Create Syllabus
+              <CloseButton toggleBox={handleSyllabusForm} fill={'#004d4d'} variant={'syllabusform'} />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
