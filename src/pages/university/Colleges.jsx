1fbc62559f6d779b46e4b0cd5aa292a049bd44e3
Krima Madhikarmi <madhikrima20@gmail.com>
2024-11-17 22:22:45 +0545

    add college button added

--- src/pages/university/Colleges.jsx
+++ src/pages/university/Colleges.jsx
@@ -7,35 +7,42 @@ const Colleges = () => {
     <>
       <Navbar />
       <PageHeader pageTitle={'Colleges'} />
-      <div className="college-present">
-        <table className="college-table">
-          <thead>
-            <tr>
-              <th>College Name</th>
-              <th>Email</th>
-              <th>Password</th>
-              <th>Activity</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Samriddhi College</td>
-              <td>samriddhi@college.com</td>
-              <td>samriddhi123</td>
-              <td className="activity-button">
-                <button className="view-button">Edit</button>
-              </td>
-            </tr>
-            <tr>
-              <td>Samriddhi College</td>
-              <td>samriddhi@college.com</td>
-              <td>samriddhi123</td>
-              <td className="activity-button">
-                <button className="view-button">Edit</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="college-box">
+        <div className='college-button'>
+          <button className="add-college-button">
+            Add College
+          </button>
+        </div>
+        <div className="college-present">
+          <table className="college-table">
+            <thead>
+              <tr>
+                <th>College Name</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Activity</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Samriddhi College</td>
+                <td>samriddhi@college.com</td>
+                <td>samriddhi123</td>
+                <td className="activity-button">
+                  <button className="view-button">Edit</button>
+                </td>
+              </tr>
+              <tr>
+                <td>Samriddhi College</td>
+                <td>samriddhi@college.com</td>
+                <td>samriddhi123</td>
+                <td className="activity-button">
+                  <button className="view-button">Edit</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
