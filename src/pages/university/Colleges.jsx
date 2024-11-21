e8f4c671ad00a5f2350b7eb4bfeecf65440d46d7
Krima Madhikarmi <madhikrima20@gmail.com>
2024-11-21 06:56:04 +0545

    add college for added

--- src/pages/university/Colleges.jsx
+++ src/pages/university/Colleges.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import PageHeader from '../../components/common/PageHeader';
 import Navbar from '../../components/Navbar';
 import '../../styles/College.css';
+import FormHeader from '../../components/common/FormHeader';
+import CustomFormField from '../../components/customFormField';
+import ButtonGroup from '../../components/common/ButtonGroup';
 
 const Colleges = () => {
+  const [collegePopup, setCollegePopUp] = useState(false);
+
+  const handleCollegePopUp = () => {
+    setCollegePopUp(!collegePopup);
+  };
+
   return (
     <>
       <Navbar />
       <PageHeader pageTitle={'Colleges'} />
       <div className="college-box">
-        <div className='college-button'>
-          <button className="add-college-button">
+        <div className="college-button">
+          <button className="add-college-button" onClick={handleCollegePopUp}>
             Add College
           </button>
         </div>
@@ -44,6 +54,55 @@ const Colleges = () => {
           </table>
         </div>
       </div>
+
+      {collegePopup && (
+        <div className="form-overlay">
+          <div className="form-design" onClick={(e) => e.stopPropagation()}>
+            <FormHeader handleForm={handleCollegePopUp} title={'Add College'} />
+            <div>
+              <form>
+                <CustomFormField
+                  label={'College Name'}
+                  name={'CollegeName'}
+                  type={'text'}
+                  placeholder={'Enter College name'}
+                />
+                <CustomFormField
+                  label={'Username'}
+                  name={'username'}
+                  type={'email'}
+                  placeholder={'Enter College Username'}
+                />
+                <CustomFormField
+                  label={'Password'}
+                  name={'CollegePassword'}
+                  type={'password'}
+                  placeholder={'Enter Password'}
+                />
+                <CustomFormField
+                  label={'Address'}
+                  name={'CollegeAddress'}
+                  type={'text'}
+                  placeholder={'Enter College Address'}
+                />
+                <CustomFormField
+                  label={'Email'}
+                  name={'CollegeEmail'}
+                  type={'email'}
+                  placeholder={'Enter College email'}
+                />
+                <CustomFormField
+                  label={'Phone Number'}
+                  name={'CollegeNumber'}
+                  type={'tel'}
+                  placeholder={'Enter College Phone Number'}
+                />
+              </form>
+              <ButtonGroup handleClose={handleCollegePopUp}/>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
