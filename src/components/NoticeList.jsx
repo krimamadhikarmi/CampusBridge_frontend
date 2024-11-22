2fe1772e61b039a8957d7b1061ece37f7545fc83
Krima Madhikarmi <madhikrima20@gmail.com>
2024-11-22 18:16:02 +0545

    edit form component seperated

--- src/components/NoticeList.jsx
+++ src/components/NoticeList.jsx
@@ -1,10 +1,9 @@
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState, useEffect } from 'react';
-import FormHeader from './common/FormHeader';
-import ButtonGroup from './common/ButtonGroup';
-import CustomFormField from './customFormField';
+
 import ConfirmPopup from './LogoutPopup';
+import EditNotice from './notice/EditNotice';
 const NoticeList = ({ index, id, title, content, category, date, getCheckboxOptions }) => {
   const [showEdit, setShowEdit] = useState(false);
   const [currentDate, setCurrentDate] = useState('');
@@ -40,41 +39,14 @@ const NoticeList = ({ index, id, title, content, category, date, getCheckboxOpti
       </div>
 
       {showEdit && (
-        <div className="form-overlay">
-          <div className="form-design">
-            <FormHeader handleForm={handleEditForm} title={'Edit Notice'} />
-            <form>
-              <CustomFormField
-                name={'NoticeId'}
-                label={'Notice Id'}
-                placeholder={'Enter the Notice Id'}
-                type={'text'}
-                value={id}
-              />
-              <CustomFormField
-                name={'Title'}
-                label={'Title'}
-                placeholder={'Enter the title of notice'}
-                type={'text'}
-                value={title}
-              />
-              <CustomFormField
-                name={'Description'}
-                label={'Description'}
-                placeholder={'Enter the Notice Description'}
-                type={'text'}
-                value={content}
-              />
-              <div className="check-group">
-                <p>Who is your notice directed to?</p>
-                <div className="check-group-item">{getCheckboxOptions()}</div>
-              </div>
-
-              <CustomFormField label={'Date'} name={'date'} type={'date'} value={currentDate} />
-              <ButtonGroup handleClose={handleEditForm} />
-            </form>
-          </div>
-        </div>
+        <EditNotice
+          handleEditForm={handleEditForm}
+          id={id}
+          title={title}
+          content={content}
+          getCheckboxOptions={getCheckboxOptions}
+          currentDate={currentDate}
+        />
       )}
 
       {deletepop && (
