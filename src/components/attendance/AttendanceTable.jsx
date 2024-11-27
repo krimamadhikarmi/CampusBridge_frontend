2642b1717730fdb46d3206bb0a8c5038772095d2
Krima Madhikarmi <madhikrima20@gmail.com>
2024-11-27 12:38:20 +0545

    table seperated

---
+++ src/components/attendance/AttendanceTable.jsx
@@ -0,0 +1,41 @@
+const AttendanceTable = ({students,attendance,handleCheckboxChange}) => {
+  return (
+    <table className="attendance-table">
+      <thead>
+        <tr>
+          <th>S.N</th>
+          <th>Student Name</th>
+          <th>Present</th>
+          <th>Absent</th>
+        </tr>
+      </thead>
+      <tbody>
+        {students.map((student) => (
+          <tr key={student.id}>
+            <td>{student.id}</td>
+            <td>{student.name}</td>
+            <td>
+              <input
+                type="radio"
+                name={`attendance-${student.id}`}
+                value="Present"
+                checked={attendance[student.id] === 'Present'}
+                onChange={() => handleCheckboxChange(student.id, 'Present')}
+              />
+            </td>
+            <td>
+              <input
+                type="radio"
+                name={`attendance-${student.id}`}
+                value="Absent"
+                checked={attendance[student.id] === 'Absent'}
+                onChange={() => handleCheckboxChange(student.id, 'Absent')}
+              />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+export default AttendanceTable;
--- src/pages/teacher/Attendance.jsx
+++ src/pages/teacher/Attendance.jsx
@@ -1,3 +1,4 @@
+import AttendanceTable from '../../components/attendance/AttendanceTable';
 import PageHeader from '../../components/common/PageHeader';
 import Navbar from '../../components/Navbar';
 import '../../styles/Attendance.css';
@@ -50,42 +51,8 @@ const Attendance = () => {
           <p> {currentDate}</p>
         </div>
         <div className="attendance-table-container">
-          <table className="attendance-table">
-            <thead>
-              <tr>
-                <th>S.N</th>
-                <th>Student Name</th>
-                <th>Present</th>
-                <th>Absent</th>
-              </tr>
-            </thead>
-            <tbody>
-              {students.map((student) => (
-                <tr key={student.id}>
-                  <td>{student.id}</td>
-                  <td>{student.name}</td>
-                  <td>
-                    <input
-                      type="radio"
-                      name={`attendance-${student.id}`}
-                      value="Present"
-                      checked={attendance[student.id] === 'Present'}
-                      onChange={() => handleCheckboxChange(student.id, 'Present')}
-                    />
-                  </td>
-                  <td>
-                    <input
-                      type="radio"
-                      name={`attendance-${student.id}`}
-                      value="Absent"
-                      checked={attendance[student.id] === 'Absent'}
-                      onChange={() => handleCheckboxChange(student.id, 'Absent')}
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <AttendanceTable students={students} attendance={attendance} handleCheckboxChange={handleCheckboxChange}/>
+          
           <button className="submitbutton" onClick={handleSubmit}>
             Submit Attendance
           </button>
