5b392964b6f93078f480cf9f5fa40e0ceb30a5f0
Krima Madhikarmi <madhikrima20@gmail.com>
2024-12-04 21:56:05 +0545

    assignment create

--- src/components/assignment/AddAssignmentForm.jsx
+++ src/components/assignment/AddAssignmentForm.jsx
@@ -2,20 +2,21 @@ import CustomFormField from '../customFormField';
 import FormHeader from '../common/FormHeader';
 import ButtonGroup from '../common/ButtonGroup';
 import { useState } from 'react';
-const AddAssignmentForm = ({handleAddForm,currentDate}) => {
-  const [AssignmentId,setAssignment]=useState('');
-  const [Question,setQuestion]=useState('');
-  const [CourseId,setCourseId]=useState('');
-  const [AssignedDate,setAssignedDate] = useState('');
-  const [SubmissionDate,setSubmissionDate]=useState('');
-  const [TeacherId,setTeacherId]=useState('');
+import axios from 'axios';
+const AddAssignmentForm = ({ handleAddForm, currentDate }) => {
+  const [AssignmentId, setAssignment] = useState('');
+  const [Question, setQuestion] = useState('');
+  const [CourseId, setCourseId] = useState('');
+  const [AssignedDate, setAssignedDate] = useState('');
+  const [SubmissionDate, setSubmissionDate] = useState('');
+  const [TeacherId, setTeacherId] = useState('');
 
-  const[file,setFile]=useState(null);
+  const [file, setFile] = useState(null);
 
-  const handleSubmit=(event)=>{
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    const formData=new FormData();
+    const formData = new FormData();
 
     formData.append('AssignmentId', AssignmentId);
     formData.append('Question', Question);
@@ -23,21 +24,31 @@ const AddAssignmentForm = ({handleAddForm,currentDate}) => {
     formData.append('AssignedDate', AssignedDate);
     formData.append('SubmissionDate', SubmissionDate);
     formData.append('TeacherId', TeacherId);
-  
+
     if (file) {
-      formData.append('File', file); 
+      formData.append('File', file);
     }
     console.log('Form submitted successfully!');
+    //checking formData files
+    for (let pair of formData.entries()) {
+      console.log(`${pair[0]}:`, pair[1]);
+    }
 
-    console.log(formData,AssignedDate,CourseId,SubmissionDate,TeacherId,Question)
+    try {
+      const response = axios.post('https://localhost:7276/api/Assignment/CreateAssignment', JSON.stringify(formData), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log('assignment', response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-  }
-
-  const handleFileChange=(e)=>{
-      setFile(e.target.files[0])
-  }
-
-
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
   return (
     <>
@@ -50,25 +61,48 @@ const AddAssignmentForm = ({handleAddForm,currentDate}) => {
               name={'AssignmentId'}
               placeholder={'Enter the assignment id'}
               type={'text'}
-              onChange={(e)=>setAssignment(e.target.value)}
+              onChange={(e) => setAssignment(e.target.value)}
             />
-            <CustomFormField label={'Question'} name={'Question'} placeholder={'Enter the question'} type={'text'}
-                          onChange={(e)=>setQuestion(e.target.value)}
-                          />
-            <CustomFormField label={'File'} name={'File'} placeholder={'Upload the file'} type={'file'}  onChange={handleFileChange}/>
-            <CustomFormField label={'Course Id'} name={'CourseId'} placeholder={'Enter the course id'} type={'text'}               onChange={(e)=>setCourseId(e.target.value)}
+            <CustomFormField
+              label={'Question'}
+              name={'Question'}
+              placeholder={'Enter the question'}
+              type={'text'}
+              onChange={(e) => setQuestion(e.target.value)}
             />
-            <CustomFormField label={'Assigned Date'} name={'AssignedDate'} type={'date'} value={currentDate}               onChange={(e)=>setAssignedDate(e.target.value)}
- />
-            <CustomFormField label={'Submission Date'} name={'SubmissionDate'} type={'date'}               onChange={(e)=>setSubmissionDate(e.target.value)}
- />
+            <CustomFormField
+              label={'File'}
+              name={'File'}
+              placeholder={'Upload the file'}
+              type={'file'}
+              onChange={handleFileChange}
+            />
+            <CustomFormField
+              label={'Course Id'}
+              name={'CourseId'}
+              placeholder={'Enter the course id'}
+              type={'text'}
+              onChange={(e) => setCourseId(e.target.value)}
+            />
+            <CustomFormField
+              label={'Assigned Date'}
+              name={'AssignedDate'}
+              type={'date'}
+              value={currentDate}
+              onChange={(e) => setAssignedDate(e.target.value)}
+            />
+            <CustomFormField
+              label={'Submission Date'}
+              name={'SubmissionDate'}
+              type={'date'}
+              onChange={(e) => setSubmissionDate(e.target.value)}
+            />
             <CustomFormField
               label={'Teacher Id'}
               placeholder={'Enter the teacher id'}
               name={'TeacherId'}
               type={'text'}
-              onChange={(e)=>setTeacherId(e.target.value)}
-
+              onChange={(e) => setTeacherId(e.target.value)}
             />
             <ButtonGroup handleClose={handleAddForm} />
           </form>
