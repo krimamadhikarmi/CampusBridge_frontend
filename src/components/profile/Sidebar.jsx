6c98433883376e99bf6e3d9017226b1f8c0ea79f
SarinaStha0 <shresthasarin71@gmail.com>
2025-01-29 19:45:39 +0545

    result and club fx

--- src/components/profile/Sidebar.jsx
+++ src/components/profile/Sidebar.jsx
@@ -4,7 +4,7 @@ const Sidebar=({isActiveTab,setIsActiveTab})=>{
     <div className="sidebar">
       <p className={isActiveTab==='General'?'active': ''} onClick={()=> setIsActiveTab('General')}>General</p>
       <p className={isActiveTab==='Attendance'?'active': '' } onClick={()=> setIsActiveTab('Attendance')}>Attendance</p>
-      <p  className={isActiveTab==='My Clubs'?'active': '' } onClick={()=> setIsActiveTab('My Clubs')}>My Clubs</p>
+      {/* <p  className={isActiveTab==='My Clubs'?'active': '' } onClick={()=> setIsActiveTab('My Clubs')}>My Clubs</p> */}
       <p  className={isActiveTab==='Account'?'active': '' } onClick={()=> setIsActiveTab('Account')}>Account</p>
       <p  className={isActiveTab==='Assignment'?'active': '' } onClick={()=> setIsActiveTab('Assignment')}>Assignment</p>
     </div>
